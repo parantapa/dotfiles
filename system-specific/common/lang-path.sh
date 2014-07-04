148e6a35e2b0fae4d59a2d35a766a02c566a386c
Parantapa Bhattacharya <pb@parantapa.net>
2014-07-04 11:29:51 +0530

    Add ruby env variable if ruby is present

--- system-specific/common/lang-path.sh
+++ system-specific/common/lang-path.sh
@@ -1,10 +1,17 @@
 # Set gem home and gem path paths and R packages
 export R_LIBS_USER="$HOME/.r-packages"
-export GEM_PATH="$(ruby -e 'puts Gem.user_dir')"
-export GEM_HOME="$GEM_PATH"
 
 # Modify path in login shell only
 if shopt -q login_shell ; then
     PATH="$HOME/.cabal/bin:$PATH"
-    PATH="$GEM_HOME/bin:$PATH"
+fi
+
+# Add ruby stuff if we have it in path
+if hash ruby 2>/dev/null ; then
+    export GEM_PATH="$(ruby -e 'puts Gem.user_dir')"
+    export GEM_HOME="$GEM_PATH"
+
+    if shopt -q login_shell ; then
+        PATH="$GEM_HOME/bin:$PATH"
+    fi
 fi
