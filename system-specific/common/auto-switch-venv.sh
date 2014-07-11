d93fa4446f54f10ddbf032e8ab4c7ddb6e88c80c
Parantapa Bhattacharya <pb@parantapa.net>
2014-07-11 16:13:51 +0530

    Bugfix auto venv switcher

--- system-specific/common/auto-switch-venv.sh
+++ system-specific/common/auto-switch-venv.sh
@@ -7,21 +7,21 @@ auto_switch_venv () {
     if [[ -z "$venv" ]] ; then
         return
     fi
-    hash deactivate 2>/dev/null
     local xvenv="$( basename "$VIRTUAL_ENV" )"
+    hash deactivate 2>/dev/null
     if [[ "$?" -eq 0 ]] && [[ -n "$xvenv" ]] ; then
         if [[ "$SWITCH_VENV_AUTO" == "$xvenv" ]] && [[ "$venv" != "$xvenv" ]]; then
             workon "$venv"
             local yvenv="$( basename "$VIRTUAL_ENV" )"
             if [[ "$yvenv" == "$venv" ]] ; then
-                export SWITCH_VENV_AUTO="$venv"
+                SWITCH_VENV_AUTO="$venv"
             fi
         fi
     else
         workon "$venv"
         local yvenv="$( basename "$VIRTUAL_ENV" )"
         if [[ "$yvenv" == "$venv" ]] ; then
-            export SWITCH_VENV_AUTO="$venv"
+            SWITCH_VENV_AUTO="$venv"
         fi
     fi
 }
