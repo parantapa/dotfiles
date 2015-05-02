9c23254829e149a954f5c08a23adb42f89c4b405
Parantapa Bhattacharya <pb@parantapa.net>
2015-05-02 21:50:23 +0530

    Change the git aliases

--- bashrc.d/git-alias.sh
+++ bashrc.d/git-alias.sh
@@ -1,11 +1,12 @@
 # Git shortcuts
 alias ga="git add"
-alias gc="git commit"
 alias gd="git diff"
-alias gdt="git difftool"
-alias gca="git commit -a"
 alias gs="git status"
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias gc="git commit"
+alias gca="git commit -a"
+alias gc.="git commit -a -m ."
 
 alias gclone="git clone"
 alias gpull="git pull"
