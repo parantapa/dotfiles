ee67d5977782c9963e84194e5fddd5074554c3a7
Parantapa Bhattacharya <pb@parantapa.net>
2015-05-20 22:39:19 +0530

    Add pdflatex xelatex aliases

--- bashrc.d/misc.sh
+++ bashrc.d/misc.sh
@@ -21,3 +21,7 @@ f () {
 e () {
     evince "$@" 2>/dev/null &
 }
+
+# Pdflatex alias
+alias xelatex="xelatex -file-line-error -halt-on-error"
+alias pdflatex="pdflatex -file-line-error -halt-on-error"
