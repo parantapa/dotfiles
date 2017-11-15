f6b34fb0b0e4bcb7c61b07784ca6a8ba6f24c244
Parantapa Bhattacharya <paran@vt.edu>
2017-11-15 17:46:22 -0500

    Clean default config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,18 +5,15 @@ set -x HOME_SDOCS "$HOME/sdocs"
 source "$HOME_DOTFILES/fishrc.d/basic.fish"
 source "$HOME_DOTFILES/fishrc.d/dotfiles.fish"
 
-# source "$HOME_DOTFILES/bashrc.d/fab_completion.sh"
 source "$HOME_DOTFILES/fishrc.d/git-alias.fish"
 source "$HOME_DOTFILES/fishrc.d/misc.fish"
-
-source "$HOME_DOTFILES/fishrc.d/kgp-proxies.fish"
-source "$HOME_DOTFILES/fishrc.d/lang-path.fish"
-
-source "$HOME_DOTFILES/fishrc.d/virtualfish-arch.fish"
+# source "$HOME_DOTFILES/fishrc.d/lang-path.fish"
+# source "$HOME_DOTFILES/bashrc.d/fab_completion.sh"
 
 set -x MY_SOUND_SYSTEM pulse
 set -x _JAVA_OPTIONS "-Dawt.useSystemAAFontSettings=gasp -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
 
-alias ssh-add-twitter-app "ssh-add $HOME_SDOCS/dotfiles-x/ssh-keys/labda-key-rsa"
-
-set -x PATH "/home/parantapa/workspace/pypb/bin" $PATH
+# source "$HOME/anaconda3/etc/fish/conf.d/conda.fish"
+#
+# Do not use anaconda provided clear
+# alias clear=/usr/bin/clear
