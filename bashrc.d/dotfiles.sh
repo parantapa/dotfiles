2429ce884f32595da415017a8ceb0822a13ed3ed
Parantapa Bhattacharya <pb@parantapa.net>
2019-06-12 17:49:29 -0400

    Remove pprint-data shortcuts

--- bashrc.d/dotfiles.sh
+++ bashrc.d/dotfiles.sh
@@ -3,10 +3,3 @@
 # Add dotfiles/bin to path
 pathadd "$HOME_DOTFILES/bin"
 pathadd "$HOME_DOTFILES/cmd-compiled"
-
-# Add the pprint-data aliases
-alias pq="pprint-data pickle"
-alias yq="pprint-data yaml"
-alias jqx="pprint-data json"
-alias mq="pprint-data msgpack"
-alias dq="pprint-data dset"
