15d235518b8072e226514ac8020402dc4adf7011
Parantapa Bhattacharya <pb@parantapa.net>
2019-08-26 11:12:30 -0400

    Add local jupyter startup script

--- bin/local-jupyter.sh
+++ bin/local-jupyter.sh
@@ -11,9 +11,6 @@ fi
 if [[ -z "$JUPYTER_IP" ]] ; then
     JUPYTER_IP=localhost
 fi
-if [[ -z "$NOTEBOOK_DIR" ]] ; then
-    NOTEBOOK_DIR="$HOME"
-fi
 
 export CONDA_ENV=notebook_env
 
@@ -24,5 +21,4 @@ exec conda-exec \
         --NotebookApp.token="" \
         --NotebookApp.password="" \
         --NotebookApp.disable_check_xsrf=True \
-        --notebook-dir "$NOTEBOOK_DIR" \
         --no-browser
