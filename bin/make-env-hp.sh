9ef4bc4b36a8b5ea5b6b39845b74bea5ba592b50
Parantapa Bhattacharya <pb@parantapa.net>
2019-09-18 12:01:55 -0400

    Update make-env-hp.sh

--- bin/make-env-hp.sh
+++ bin/make-env-hp.sh
@@ -15,30 +15,29 @@ CONDA_ENV=hp
 
 conda remove -n "$CONDA_ENV" --all
 
-conda create -y -n "$CONDA_ENV"
-
-conda activate "$CONDA_ENV"
-
-conda install -y \
+conda create -y -n "$CONDA_ENV" \
     python=3.7 ipykernel \
     numpy scipy matplotlib pandas \
-    statsmodels scikit-learn \
-    pyarrow \
-    nltk \
     networkx \
-    dask
+    statsmodels scikit-learn scikit-image \
+    nltk spacy \
+    pyarrow dask
+
+conda activate "$CONDA_ENV"
 
 conda install -y \
     pytorch torchvision cudatoolkit=9.0 -c pytorch
 
 pip install \
+    black pylint pyflakes pydocstyle \
+    click logbook \
     tqdm \
     pyyaml toml \
     bokeh seaborn altair \
     xlrd \
     glom \
     more-itertools \
-    tensorFlow-gpu \
+    tensorflow-gpu \
     tensorboardx
 
 # conda clean -tipsy
