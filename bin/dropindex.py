5e4e4e5015ed8a1e88dfe45798f2db8b076db0ab
Parantapa Bhattacharya <pb@parantapa.net>
2020-01-18 16:46:16 +0530

    Add dropindex.py script

---
+++ bin/dropindex.py
@@ -0,0 +1,32 @@
+"""Drop all indexes in a sqlite3 file."""
+
+import sqlite3
+import click
+
+
+@click.command()
+@click.option(
+    "-f",
+    "--file",
+    "fname",
+    type=click.Path(exists=True),
+    required=True,
+    help="Path of the sqlite3 file",
+)
+def main(fname):
+    """Drop all indexes in a sqlite3 file."""
+    con = sqlite3.connect(fname)
+    sql = "SELECT name FROM sqlite_master WHERE type == 'index'"
+    cur = con.execute(sql)
+    indexes = [i for i, in cur]
+
+    sql = "DROP INDEX %s"
+    for index in indexes:
+        print(sql % index)
+        con.execute(sql % index)
+
+    sql = "VACUUM"
+    con.execute(sql)
+
+if __name__ == "__main__":
+    main()
