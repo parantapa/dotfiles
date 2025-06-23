ee22a611868c44b8477d0bfd2b2acddc2818da4b
Parantapa Bhattacharya <pb@parantapa.net>
2025-06-23 08:46:16 -0400

    Add ESL tree sitter setup

--- config/nvim/lua/config/plugins.lua
+++ config/nvim/lua/config/plugins.lua
@@ -229,6 +229,19 @@ require("nvim-treesitter.configs").setup({
     }
 })
 
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.esl = {
+    install_info = {
+        url = "~/workspace/tree-sitter-esl",    -- local path or git repo
+        files = { "src/parser.c" },             -- note that some parsers also require src/scanner.c or src/scanner.cc
+        -- optional entries:
+        branch = "main",                        -- default branch in case of git repo if different from master
+        generate_requires_npm = false,          -- if stand-alone parser without npm dependencies
+        requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    },
+    filetype = "esl",                           -- if filetype does not match the parser name
+}
+
 -- Rainbow Delimiters {{{1
 
 vim.g.rainbow_delimiters = {
